4c0535f3f879a5799aae8f9681d7d0b03479f521
MostafaMohamed2001 <mostafamohamed8828@gmail.com>
2024-07-08 00:58:51 +0300

    sCommit

--- .history/config/mongoose_20240708005625.js
+++ .history/config/mongoose_20240708005625.js
@@ -1,9 +1,8 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
- 
-
-    await mongoose.connect('mongodb+srv://mostafabusnins8828:MGFG1fZ7KhUapndR@cluster0.5kywrxu.mongodb.net/authGoogle?retryWrites=true&w=majority').then((conn) => {
+  
+    await mongoose.connect('mongodb://localhost:27017/authGoogle').then((conn) => {
       console.log(`Connected to DB => ${conn.connection.name}`);
       console.log(`Connected to DB on host => ${conn.connection.host}`);
     }).catch((err) => {
@@ -13,3 +12,4 @@ const connectDB = async () => {
   }
 
 module.exports = connectDB;
+DB_URI=
--- .history/config/mongoose_20240708005631.js
+++ .history/config/mongoose_20240708005631.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
- 
+  mongodb+srv://mostafabusnins8828:MGFG1fZ7KhUapndR@cluster0.5kywrxu.mongodb.net/ecommerceApi?retryWrites=true&w=majority
 
-    await mongoose.connect('mongodb+srv://mostafabusnins8828:MGFG1fZ7KhUapndR@cluster0.5kywrxu.mongodb.net/authGoogle?retryWrites=true&w=majority').then((conn) => {
+    await mongoose.connect('mongodb://localhost:27017/authGoogle').then((conn) => {
       console.log(`Connected to DB => ${conn.connection.name}`);
       console.log(`Connected to DB on host => ${conn.connection.host}`);
     }).catch((err) => {
